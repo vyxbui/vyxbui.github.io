f71822ad6e9c15333c57b8895057151ee2a83f0b
Vy Bui <vyxbui@gmail.com>
2026-01-28 00:50:13 -0500

    chore: fix typo in headshot path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import './Hero.css'
 import { MdArrowDownward } from 'react-icons/md'
-import headshot from './assets/headshot.jpg'
+import headshot from 'src/assets/headshot.jpg'
 
 const ScrollDownIcon = () => {
   return (
